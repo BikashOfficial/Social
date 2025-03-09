dc8de01422034556e3865b5afd136ce9e35077b2
BikaashMeher <bm244368@gmail.com>
2025-03-10 03:16:28 +0530

    fix 2

--- fntend/vite.config.js
+++ fntend/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true
   }
 })
